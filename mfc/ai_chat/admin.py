bc48146f5d7f87a6c68a97db8e25700f55391eec
kirill201 <kirill.301522@gmail.com>
2023-08-26 20:18:18 +1000

    [DJANGO] refactor

--- mfc/ai_chat/admin.py
+++ mfc/ai_chat/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import QuestionAnswering
 
+
 # Register your models here.
 
 class QuestionAnsweringAdmin(admin.ModelAdmin):
     list_display = ('question', 'answer')
     search_fields = ('question',)
+
 
 admin.site.register(QuestionAnswering, QuestionAnsweringAdmin)
--- mfc/ai_chat/views.py
+++ mfc/ai_chat/views.py
@@ -13,9 +13,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 from torch.utils.data import Dataset, DataLoader
 
-
 model = torch.load(r"C:\Users\jet\Desktop\pythonProject3\ruBert.pt")
 tokenizer = torch.load(r"C:\Users\jet\Desktop\pythonProject3\tokenizer.pt")
+
 
 class CustomDataset(Dataset):
 
@@ -33,6 +33,7 @@ class CustomDataset(Dataset):
         output = self.tokenize(output)
         return {k: v.reshape(-1) for k, v in output.items()}
 
+
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output['last_hidden_state']
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
@@ -40,23 +41,25 @@ def mean_pooling(model_output, attention_mask):
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     return sum_embeddings / sum_mask
 
+
 def get_embedding(quary):
-  eval_ds = CustomDataset(quary)
-  eval_dataloader = DataLoader(eval_ds, batch_size=1)
+    eval_ds = CustomDataset(quary)
+    eval_dataloader = DataLoader(eval_ds, batch_size=1)
 
-  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-  model.to(device)
-  model.eval()
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model.to(device)
+    model.eval()
 
-  embeddings = torch.Tensor().to(device)
+    embeddings = torch.Tensor().to(device)
 
-  with torch.no_grad():
-      for n_batch, batch in enumerate(tqdm(eval_dataloader)):
-          batch = {k: v.to(device) for k, v in batch.items()}
-          outputs = model(**batch)
-          embeddings = torch.cat([embeddings, mean_pooling(outputs, batch['attention_mask'])])
-      embeddings = embeddings.cpu().numpy()
-  return embeddings
+    with torch.no_grad():
+        for n_batch, batch in enumerate(tqdm(eval_dataloader)):
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            embeddings = torch.cat([embeddings, mean_pooling(outputs, batch['attention_mask'])])
+        embeddings = embeddings.cpu().numpy()
+    return embeddings
+
 
 # df = pd.read_excel(r'C:\Users\jet\Desktop\pythonProject3\train_dataset_Датасет.xlsx')
 #
@@ -113,10 +116,13 @@ def get_embedding(quary):
 def get_embeddings_from_db():
     return QuestionAnswering.objects.all().values_list('question', 'answer')
 
+
 embedding_entries = get_embeddings_from_db()
 questions_from_db = [entry[0] for entry in embedding_entries]
 print(questions_from_db)
 quastions_embeddings = []
+
+
 # for entry in embedding_entries:
 #     byte_data = entry[1]  # Замените 1 на индекс вашего байтового объекта в кортеже
 #     embedding = np.frombuffer(byte_data, dtype=np.float32)
@@ -125,6 +131,7 @@ quastions_embeddings = []
 
 def ai_chat(request):
     return render(request, "ai_chat/chat.html")
+
 
 def ai_request(request):
     body = json.loads(request.body)
